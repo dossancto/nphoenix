25bc3a982dee3f88dcc2188e65de05cd8b4017a0
dossancto <dossantossilvalucas963@gmail.com>
2025-08-25 21:56:14 -0300

    add testing project

---
+++ working/content/PhoenixApi/tests/PhoenixApi.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PhoenixApi.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ working/content/PhoenixApi/tests/PhoenixApi.Tests.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PhoenixApi.Tests.Shared;
+
+public class Class1
+{
+
+}
