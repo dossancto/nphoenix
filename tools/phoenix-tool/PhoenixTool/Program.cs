d1de11d4edb5e0019906e393edba8d12b3887e03
dossancto <dossantossilvalucas963@gmail.com>
2025-08-27 20:13:35 -0300

    remove not suported command

--- tools/phoenix-tool/PhoenixTool/Program.cs
+++ tools/phoenix-tool/PhoenixTool/Program.cs
@@ -13,36 +13,6 @@ var builder = CoconaApp.CreateBuilder();
 var app = builder.Build();
 
 app.AddCommand(
-    "gen.api",
-    (
-     [Argument(Description = "The App module")] string module,
-     [Argument(Description = "The entity to be used")] string entity,
-     [Argument(Description = "Fields")] List<string> fields,
-     [Option(Description = "The path to the project")] string? path
-    ) =>
-    {
-        var choosedFile = CommandHelpers.GetProjectPath(path);
-
-        if (choosedFile is null)
-        {
-            AnsiConsole.MarkupLine("[red]No .csproj file found in the current directory[/]");
-            return;
-        }
-
-        var projectMetadata = ProjectMetadata.FromCsProjPath(choosedFile);
-        var scaffoldInput = new ScaffoldInput(
-            Module: module,
-            Entity: entity,
-            Fields: fields
-        );
-
-        var res = GenerateEntityFactory.Generate(scaffoldInput, projectMetadata);
-
-        Console.WriteLine(res);
-    })
-    .WithDescription("Gen API endpoint");
-
-app.AddCommand(
     "gen.scheme",
     (
      [Argument(Description = "The App module")] string module,
