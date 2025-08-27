5c6b2e7ad2f8124064246b9c28d595940b8d307e
dossancto <dossantossilvalucas963@gmail.com>
2025-08-27 20:13:16 -0300

    generate module

--- tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Usecases/GenerateModuleUsecase.cs
+++ tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Usecases/GenerateModuleUsecase.cs
@@ -1,4 +1,6 @@
 using PhoenixTool.App.Generate.Commum.Dtos;
+using PhoenixTool.App.Generate.Commum.Extensions;
+using PhoenixTool.App.Generate.Commum.Factories;
 
 namespace PhoenixTool.App.Generate.Commum.Usecases;
 
@@ -11,6 +13,23 @@ public static class GenerateModuleUsecase
 {
     public static GenerateModuleUsecaseOutput TryCreateModule(ScaffoldInput input, ProjectMetadata metadata, string pathToModule)
     {
-        return new(true);
+        var formatedModuleName = StringExtensions.ToPascalCase(input.Module);
+
+        var pathToModuleFile = Path.Combine(pathToModule, $"{formatedModuleName}Module" + ".cs");
+
+        var moduleAlreadyExists = Path.Exists(pathToModuleFile);
+
+        if (moduleAlreadyExists)
+        {
+            return new(true);
+        }
+
+        Directory.CreateDirectory(pathToModule);
+
+        var moduleContent = GenerateModuleFactory.Generate(input, metadata);
+
+        File.WriteAllText(pathToModuleFile, moduleContent);
+
+        return new(false);
     }
 }
