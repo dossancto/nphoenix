0e8ecd92915f8014a3c4a69a8b0702c581bff335
dossancto <dossantossilvalucas963@gmail.com>
2025-08-27 11:09:36 -0300

    parse field type from text

---
+++ tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Dtos/FieldTypeDefinition.cs
@@ -0,0 +1,51 @@
+namespace PhoenixTool.App.Generate.Commum.Dtos;
+
+public record FieldTypeDefinition
+(
+    string FieldName,
+    string TypeName,
+    bool IsNullable,
+    int MaxLength
+)
+{
+  public static FieldTypeDefinition FromRawString(string raw)
+  {
+    var tokens = raw.Split(":");
+
+    if (tokens.Length < 2)
+    {
+      throw new ArgumentException("Invalid raw string");
+    }
+
+    var fieldName = tokens[0];
+    var typeName = tokens[1];
+    var isNullable = false;
+    var maxLenghtParam = tokens.ElementAtOrDefault(2);
+
+    var maxLength = 0;
+
+    if (maxLenghtParam is not null)
+    {
+      if (!int.TryParse(maxLenghtParam, out var maxLengthParsed))
+      {
+        throw new ArgumentException("Invalid max length, should be a number");
+      }
+
+      maxLength = maxLengthParsed;
+    }
+
+    if (typeName.EndsWith('?'))
+    {
+      typeName = typeName.Remove(typeName.Length - 1);
+      isNullable = true;
+    }
+
+
+    return new(
+        fieldName,
+        typeName,
+        isNullable,
+        maxLength
+    );
+  }
+}
