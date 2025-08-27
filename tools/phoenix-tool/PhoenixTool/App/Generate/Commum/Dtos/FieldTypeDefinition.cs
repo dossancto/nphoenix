0e6f590ebbe2e673fc4f803f62b45ceb8fc1df0e
dossancto <dossantossilvalucas963@gmail.com>
2025-08-27 20:29:46 -0300

    validate data type inputs

--- tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Dtos/FieldTypeDefinition.cs
+++ tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Dtos/FieldTypeDefinition.cs
@@ -8,6 +8,25 @@ public record FieldTypeDefinition
     int MaxLength
 )
 {
+    private static readonly List<string> ValidDataTypes = [
+        "string",
+        "int",
+        "bool",
+        "decimal",
+        "DateTime",
+        "Guid",
+        "byte",
+        "sbyte",
+        "short",
+        "ushort",
+        "long",
+        "ulong",
+        "float",
+        "double",
+        "char",
+        "object",
+    ];
+
     public static FieldTypeDefinition FromRawString(string raw)
     {
         var tokens = raw.Split(":");
@@ -22,6 +41,17 @@ public record FieldTypeDefinition
         var isNullable = false;
         var maxLenghtParam = tokens.ElementAtOrDefault(2);
 
+        if (typeName.EndsWith('?'))
+        {
+            typeName = typeName.Remove(typeName.Length - 1);
+            isNullable = true;
+        }
+
+        if (!ValidDataTypes.Contains(typeName))
+        {
+            throw new ArgumentException($"Invalid type name: {typeName}");
+        }
+
         var maxLength = 0;
 
         if (maxLenghtParam is not null)
@@ -32,12 +62,6 @@ public record FieldTypeDefinition
             }
 
             maxLength = maxLengthParsed;
-        }
-
-        if (typeName.EndsWith('?'))
-        {
-            typeName = typeName.Remove(typeName.Length - 1);
-            isNullable = true;
         }
 
         return new(
