c443b1ea7de4279887061f347e060c9841a646f2
dossancto <dossantossilvalucas963@gmail.com>
2025-08-27 20:02:48 -0300

    format file names

--- tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Usecases/GenerateSchemeUsecase.cs
+++ tools/phoenix-tool/PhoenixTool/App/Generate/Commum/Usecases/GenerateSchemeUsecase.cs
@@ -1,4 +1,5 @@
 using PhoenixTool.App.Generate.Commum.Dtos;
+using PhoenixTool.App.Generate.Commum.Extensions;
 using PhoenixTool.App.Generate.Commum.Factories;
 
 namespace PhoenixTool.App.Generate.Commum.Usecases;
@@ -15,12 +16,14 @@ public static class GenerateSchemeUsecAse
 
         var entityConfiguration = EntityConfigurationFactory.Create(input, metadata);
 
+        var formatedName = StringExtensions.ToPascalCase(input.Entity);
+
         Directory.CreateDirectory(pathToModule);
         Directory.CreateDirectory(pathToEntities);
         Directory.CreateDirectory(pathToEntityConfiguration);
 
-        File.WriteAllText(Path.Combine(pathToEntities, $"{input.Entity}.cs"), entityContent);
-        File.WriteAllText(Path.Combine(pathToEntityConfiguration, $"{input.Entity}EntityConfiguration.cs"), entityConfiguration);
+        File.WriteAllText(Path.Combine(pathToEntities, $"{formatedName}.cs"), entityContent);
+        File.WriteAllText(Path.Combine(pathToEntityConfiguration, $"{formatedName}EntityConfiguration.cs"), entityConfiguration);
 
         var moduleContentOutput = GenerateModuleUsecase.TryCreateModule(input, metadata, pathToModule);
     }
